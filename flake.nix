21964b0550556e217b0770e908bf8a68edec710b
ebaron <evanbaron.a4@gmail.com>
2025-07-13 11:53:40 +0200

    Initial NixOS Configuration

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = { 
+        monolith = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/monolith/configuration.nix
+          ];
+        };
+
+	nomad = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+	  modules = [
+            ./hosts/nomad/configuration.nix
+	  ];
+	};
+    };
+  };
+}
